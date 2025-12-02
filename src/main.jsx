716566820d45682b8284920c226f44afe9439c1c
Caleb-Aranda-Vela <caleb.aranda@hemoeco.com>
2025-12-01 19:49:26 -0600

    Update comment for Tailwind CSS import line

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LandingPage from './LandingPage.jsx';
-import './index.css'; // Esto importa tus estilos de Tailwind
+import './index.css'; // <--- ¡Esta línea importa tus estilos de Tailwind!
 
 // Aquí se 'monta' la aplicación dentro del <div id="root"> del index.html
 ReactDOM.createRoot(document.getElementById('root')).render(
